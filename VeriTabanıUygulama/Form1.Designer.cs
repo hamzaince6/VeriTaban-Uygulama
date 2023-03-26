504473c66dc36f1eca529f1a4b6f4e3af93fe5a7
halte <halte@DESKTOP-6FDKK2M>
2023-03-26 15:32:42 +0300

    Add project files.

---
+++ VeriTabanıUygulama/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace VeriTabanıUygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnlistele = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(964, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Aç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(964, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kapat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnlistele});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(687, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnlistele
+            // 
+            this.btnlistele.Image = ((System.Drawing.Image)(resources.GetObject("btnlistele.Image")));
+            this.btnlistele.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnlistele.Name = "btnlistele";
+            this.btnlistele.Size = new System.Drawing.Size(89, 22);
+            this.btnlistele.Text = "Kayıt Listele";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(663, 410);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 567);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnlistele;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ VeriTabanıUygulama/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb; //Access Veri Tabanı Kütüphanesi
+namespace VeriTabanıUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection Connection=new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=DenemeDers.accdb");
+        //Veritabanı Kontrollü Açan Method
+        public void ConnectionOpen()
+        {
+            try
+            {
+                Connection.Open();
+            }
+            catch (Exception hata)
+            {
+                //Sistem Hata Mesajı
+                MessageBox.Show(hata.Message,"Bağlantı Açma Hata Penceresi");
+            }
+        }
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConnectionOpen();
+            MessageBox.Show("Veritabanı Açık");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Connection.Close();
+            MessageBox.Show("Veritabanı Kapalı");
+        }
+    }
+}
